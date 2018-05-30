584ad64e07b16a803202aa842b975269049bc7b8
Phili Day <pd14041@my.bristol.ac.uk>
2018-05-30 17:08:26 +0100

    Before Upgrade

--- ProjectApplication/app/src/main/java/com/philiday/projectapplication/RunActivity.java
+++ ProjectApplication/app/src/main/java/com/philiday/projectapplication/RunActivity.java
@@ -1,16 +1,40 @@
 package com.philiday.projectapplication;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class RunActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class RunActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
+
+    private GoogleMap mMap;
+    LatLng myCoordinates;
+    LocationManager locationManager;
+    final static int PERMISSION_ALL = 1;
+    final static String[] PERMISSIONS = {Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION};
+    MarkerOptions mo;
+    Marker marker;
 
 
     @Override
@@ -22,17 +46,105 @@ public class RunActivity extends AppCompatActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.mapView);
         mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mo = new MarkerOptions().position(new LatLng(0, 0)).title("Current location");
+        if (Build.VERSION.SDK_INT >= 23 && !isPermissionGranted()){
+            requestPermissions(PERMISSIONS, PERMISSION_ALL);
+        } else requestLocation();
+        if (!isLocationEnabled())
+            Log.v("mytag","No location");
+            //showAlert(1);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        // Add a marker in Sydney, Australia,
+        mMap = googleMap;
+        marker = mMap.addMarker(mo);
+        // Add a marker
         // and move the map's camera to the same location.
-        LatLng startPos = new LatLng(51.4558654, -2.6034682);
-        googleMap.addMarker(new MarkerOptions().position(startPos)
-                .title("Start Position"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(startPos));
+//        LatLng startPos = new LatLng(51.4558654, -2.6034682);
+//        googleMap.addMarker(new MarkerOptions().position(startPos)
+//                .title("Start Position"));
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(startPos));
     }
 
 
+    @Override
+    public void onLocationChanged(Location location) {
+        LatLng myCoordinates = new LatLng(location.getLatitude(), location.getLongitude());
+        marker.setPosition(myCoordinates);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myCoordinates));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    private void requestLocation() {
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.POWER_HIGH);
+        String provider = locationManager.getBestProvider(criteria, true);
+        locationManager.requestLocationUpdates(provider, 10000, 10, this);
+    }
+
+    private boolean isLocationEnabled(){
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    private boolean isPermissionGranted() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED || checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Log.v("mylog", "Permission granted");
+            return true;
+        } else {
+            Log.v("mylog", "Permission not granted");
+            return false;
+        }
+    }
+
+//    private void showAlert(final int status){
+//        String message, title, btnText;
+//        if(status = 1) {
+//            message = "Turn on location settings";
+//            title = "Enable location";
+//            btnText = "Location Settings";
+//        }
+//        else{
+//            message = "Please allow access";
+//            title = "Permission access";
+//            btnText = "Grant";
+//        }
+//
+//        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+//        dialog.setCancelable(false);
+//        dialog.setTitle(title)
+//                .setMessage(message)
+//                .setPositiveButton(btnText, onClick(paramDialogInterface, paramInt) -> {
+//            if (status == 1) {
+//                Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+//                startActivity(myIntent);
+//            } else
+//                requestPermissions(PERMISSIONS, PERMISSION_ALL);
+//
+//        })
+//        .setNegativeButton("Cancel", onClick(paramDialogInterface, paramInt) -> {
+//            finish();
+//        });
+//        dialog.show();
+//    }
 }
--- ProjectApplication/app/src/main/java/com/philiday/projectapplication/SignUpActivity.java
+++ ProjectApplication/app/src/main/java/com/philiday/projectapplication/SignUpActivity.java
@@ -3,26 +3,21 @@ package com.philiday.projectapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
-//    private TextView textView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
 
-
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = "hello";
-
-        // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView2);
-        textView.setText(message);
+    public void goToTimeline(View view) {
+        Intent intent = new Intent(this, timelineActivity.class);
+        startActivity(intent);
     }
 
 }
